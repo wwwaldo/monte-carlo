0a5ad8c90ef3b1de88ffe9724de8f3494fd6a72d
Caroline Lin <ruoyi.lin@gmail.com>
2018-12-09 18:16:40 -0500

    copy merge's main to plotting.py

---
+++ plotting.py
@@ -0,0 +1,59 @@
+from merge import * 
+
+# Literally copied from merge
+
+np.random.seed(0)
+
+if __name__ == '__main__':
+    Ns = [40]
+    nsamples_list = [400]
+
+
+    for N in Ns:
+        for nsamples in nsamples_list:
+            print(N,nsamples)
+            boundary_func = lambda x: x[0]*x[0] + x[1]*x[1] - 1
+            #boundary_func = squiggly_domain(0.45)
+            h = 2/N
+            # grid_to_vec, vec_to_grid, boundary, grid_to_point = grid_gen(boundary_func, np.array([-1.0,-1.0]), np.array([1.0,1.0]), np.array([N+1,N+1]))
+            grid_to_vec, vec_to_grid, boundary, grid_to_point = grid_gen(boundary_func, np.array([-1.5,-1.5]), np.array([1.5,1.5]), np.array([N+1,N+1]))
+
+            domain = SetWithCartesianBoundary(boundary, grid_to_point, boundary_func, h)
+            
+            f = lambda x: x[0]**3 + x[1]**3 - 3*x[0]*x[0]*x[1] - 3*x[0]*x[1]*x[1] + 1
+            g = lambda x: 0
+            # f = lambda x: x[0]**2 + x[1]**2
+            # g = lambda x: -4
+
+            simulator = MonteCarloSimulator(domain, nsamples,
+                lambda x, y: x ** 3 + y ** 3 - 3 * x ** 2 * y -3 * y ** 2 * x + 1
+            )
+
+            simulator.simulate()
+            u = simulator.solve_coupling()
+
+
+            def find_val(p):
+                return u[domain.bdry_dict[p]]
+
+            L, b = gen_L(find_val, g, grid_to_vec, vec_to_grid, boundary, grid_to_point, h)
+
+            x = sppl.spsolve(L, b)
+            x_true = np.vectorize(lambda x: f(grid_to_point(x)), signature='(n)->()')(np.array(vec_to_grid))
+            
+            print(np.max(np.abs(x-x_true)))
+            print(np.max(np.abs(x-x_true)/x_true))
+            print(np.mean(np.abs(x-x_true)/x_true))
+
+
+            find_val_exact = lambda x: f(grid_to_point(x))
+            for k in boundary:
+                boundary[k] = None
+
+            L, b = gen_L(find_val_exact, g, grid_to_vec, vec_to_grid, boundary, grid_to_point, h)
+
+            x = sppl.spsolve(L, b)
+            x_true = np.vectorize(lambda x: f(grid_to_point(x)), signature='(n)->()')(np.array(vec_to_grid))
+            print(np.max(np.abs(x-x_true)))
+            # print(np.max(np.abs(x-x_true)/x_true))
+            # print(np.mean(np.abs(x-x_true)/x_true))
